04fad87d2f137f7ae6413c22294ddd1e87715bb3
Jared Mollenkamp <buhgit@gmail.com>
2017-08-28 19:53:19 -0500

    Create hacker.py

---
+++ hacker.py
@@ -0,0 +1,32 @@
+import random
+
+d = ""
+count = 0
+while count < :
+    d = d + (str(chr(random.randrange(ord('A'),ord('Z')))))
+    count = count + 1
+
+#for pass try 741852963753951
+#on seconet then you pass you hacked into
+# by mason and jared
+
+print ("login")
+user = (input(""))
+print("pass")
+b = (input(""))
+if b == "741852963753951" :
+    print ("encriped")
+    print ("pass is ", d)
+    print ("login")
+    user = (input(""))
+    print("pass")
+    c = input("")
+    if (c == d):
+        print ("hi")
+        print (user)
+        
+    else:
+        print ("incorect")
+else:
+    print ("INCORECT")
+
