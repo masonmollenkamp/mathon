8471cebae14d77b1dae0d5ded559bda9df0c3fdc
Jared Mollenkamp <buhgit@gmail.com>
2017-09-30 13:14:44 -0500

    calculator

---
+++ calculator.py
@@ -0,0 +1,30 @@
+ecwater =(eval(input("1 = + 2 = - 3 = x 4 = %")))	 
+first_number = (eval(input("firt number")))
+two_number = (eval(input("seconed number")))
+three_number = (eval(input("do you want 3erd number 0 no 1 yes")))
+if (three_number == 1):
+  three = (eval(input("what is it")))
+if (ecwater == 1):
+   if(three_number == 1):
+     answer = (first_number + two_number)
+     print(answer + three)
+   if (three_number == 0):
+    print (first_number + two_number)
+if (ecwater == 2):
+  if(three_number == 1):
+      answer = (var(first_number + two_number))
+      print(answer + three)
+  if (three_number == 0):
+    print (first_number + two_number)
+if (ecwater == 3):
+   if(three_number == 1):
+     answer = (var(first_number + two_number))
+     print(answer + three)
+   if (three_number == 0):
+    print (first_number + two_number)
+if (ecwater == 4):
+   if(three_number == 1):
+     answer = (var(first_number + two_number))
+     print(answer + three)
+   if (three_number == 0):
+    print (first_number + two_number)
