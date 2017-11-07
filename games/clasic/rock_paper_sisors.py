c78a52af94e46df11cabcbca82613d12d2269671
mason mollenkamp <masonmollenkamp@gmail.com>
2017-11-06 18:19:42 -0600

    Create rock_paper_sisors.py

---
+++ games/clasic/rock_paper_sisors.py
@@ -0,0 +1,27 @@
+import random
+m = ["rock", "paper", "sisors"]
+com = random.choice(m)
+player = input("rock paper sisors or quit")
+while (player != "quit"):
+  if (player == "paper"):
+    if com == "sisors":
+      print("com wins")
+    elif (com == "rock"):
+      print("You win!")
+    else:
+      print("tie")
+  if (player == "sisors"):
+    if com == "rock":
+      print("com wins")
+    elif (com == "paper"):
+      print("You win!")
+    else:
+      print("tie")
+  if (player == "rock"):
+    if com == "paper":
+      print("com wins")
+    elif (com == "sisors"):
+      print("You win!")
+    else:
+      print("tie")
+  player = input("rock paper sisors or quit")
