79129165b031d828d7c80c2da4753eec6ad3dd51
mason mollenkamp <masonmollenkamp@gmail.com>
2017-11-18 12:53:57 -0600

    Update rock_paper_sisors.py

--- games/clasic/rock_paper_sisors.py
+++ games/clasic/rock_paper_sisors.py
@@ -1,8 +1,15 @@
 import random
-m = ["rock", "paper", "sisors"]
+m = ["rock", "paper", "sissors"]
 com = random.choice(m)
-player = input("rock paper sisors or quit")
+print("boost gives you 7 in 10 chance of winning")
+player = input("rock paper sissors boost or quit")
 while (player != "quit"):
+  if player == "boost":
+    input("rock paper or sissors")
+    win = random.randrange(1,10)
+    if player == "boost":
+      if win < 7:
+        print("You win!")
   com = random.choice(m)
   if (player == "paper"):
     if com == "sisors":
@@ -25,4 +32,4 @@ while (player != "quit"):
       print("You win!")
     else:
       print("tie")
-  player = input("rock paper sisors or quit")
+  player = input("rock paper sissors boost or quit")
